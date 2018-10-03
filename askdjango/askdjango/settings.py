a68884195ccc4df350d4b70480e63df59a173156
HyungtaeMoon <blessmht@gmail.com>
2018-10-03 20:53:05 +0900

    accounts 앱 생성 및 로그인 기능

--- askdjango/askdjango/settings.py
+++ askdjango/askdjango/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog',
+    'accounts',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+from django.urls import reverse_lazy
+
+LOGIN_URL = reverse_lazy('login')
+# LOGIN_REDIRECT_URL = reverse_lazy('profile')
