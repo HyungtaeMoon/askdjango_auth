71474dd08cdfc5ac6b0379e7bf8c1cfe55f509e8
HyungtaeMoon <blessmht@gmail.com>
2018-10-04 23:11:57 +0900

    signup 구현

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
