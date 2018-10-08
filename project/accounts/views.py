25297457d2adf50d43c8635fd54a2f3dcc324c98
HyungtaeMoon <blessmht@gmail.com>
2018-10-08 20:55:06 +0900

    회원가입 시에 next 인자 처리하기

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -6,20 +6,21 @@ from django.contrib.auth.models import User
 from django.views.generic import CreateView
 from .forms import SignupForm
 
-
+'''
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user) # 로그인 처리
-            return redirect('profile')
+            next_url = request.GET.get('next') or 'profile'
+            return redirect(next_url)
     else:
         form = SignupForm()
     return render(request, 'accounts/signup.html', {
         'form': form,
     })
-
+'''
 
 
 class SignupView(CreateView):
@@ -28,7 +29,8 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
     def form_valid(self, form):
         user = form.save()
