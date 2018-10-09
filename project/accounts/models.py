4a6655dc5f9237484a9fa7d716de959ff51a977d
HyungtaeMoon <blessmht@gmail.com>
2018-10-09 20:07:38 +0900

    가입환영 이메일 보내기

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.db import models
+from django.core.mail import send_mail
 
 
 class Profile(models.Model):
@@ -10,9 +11,19 @@ class Profile(models.Model):
 
 
 def on_post_save_for_user(sender, **kwargs):
+    # 가입 시기
     if kwargs['created']:
         user = kwargs['instance']
         Profile.objects.create(user=user)
+
+        # 환영 이메일 보내기
+        send_mail(
+            '환영합니다',
+            '가입해주셔서 감사합니다',
+            'from@example.com',
+            [user.email],
+            fail_silently=False,
+        )
 
 
 post_save.connect(on_post_save_for_user, sender=settings.AUTH_USER_MODEL)
