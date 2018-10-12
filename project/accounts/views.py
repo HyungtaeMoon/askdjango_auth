b1e7ea99ba2394ff0bc7e252dd38a41a078087c2
HyungtaeMoon <blessmht@gmail.com>
2018-10-12 12:38:04 +0900

    PasswordResetView를 활용하여, 이메일/URL을 통한 로그인 구현

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,8 +1,14 @@
 from django.conf import settings
-from django.contrib.auth import login as auth_login
+from django.contrib import messages
+from django.contrib.auth import login as auth_login, get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.views import PasswordResetView
+from django.core.exceptions import ValidationError
+from django.http import Http404
 from django.shortcuts import render, resolve_url, redirect
 from django.contrib.auth.models import User
+from django.utils.http import urlsafe_base64_decode
 from django.views.generic import CreateView
 from .forms import SignupForm
 
@@ -53,3 +59,27 @@ signup = CreateView.as_view(model=User,
 def profile(request):
     request.user    # django.contrib.auth.models.AnonymousUser
     return render(request, 'accounts/profile.html')
+
+
+class RequestLoginViaUrlView(PasswordResetView):
+    template_name = 'accounts/request_login_via_url_form.html'
+    title = '이메일을 통한 로그인'
+    email_template_name = 'accounts/login_via_url.html'
+    success_url = settings.LOGIN_URL
+
+
+def login_via_url(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        current_user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist, ValidationError):
+        raise Http404
+
+    if default_token_generator.check_token(current_user, token):
+        auth_login(request, current_user)
+        messages.info(request, '로그인이 승인되었습니다')
+        return redirect('root')
+
+    messages.error(request, '로그인이 거부되었습니다')
+    return redirect('root')
