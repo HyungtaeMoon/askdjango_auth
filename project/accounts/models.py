73eaec94b3bcc164b1f022eecab92ea2d7731813
HyungtaeMoon <blessmht@gmail.com>
2018-10-16 21:19:25 +0900

    Permission 시스템을 이용하여 중복 로그인 막기

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,8 +1,13 @@
+from importlib import import_module
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.signals import user_logged_in
 from django.db.models.signals import post_save
 from django.db import models
 from django.core.mail import send_mail
+
+
+SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
 
 
 class User(AbstractUser):
@@ -39,3 +44,27 @@ def on_post_save_for_user(sender, **kwargs):
 
 
 post_save.connect(on_post_save_for_user, sender=settings.AUTH_USER_MODEL)
+
+
+class UserSession(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, editable=False)
+    session_key = models.CharField(max_length=40, editable=False)
+    created_at = models.DateField(auto_now_add=True)
+
+
+def kick_my_other_session(sender, request, user, **kwargs):
+    print('kicked my other login session')
+
+    for user_session in UserSession.objects.filter(user=user):
+        session_key = user_session.session_key
+        session = SessionStore(session_key)
+        # session.delete()
+        session['kicked'] = True
+        session.save()
+        user_session.delete()
+
+    session_key = request.session.session_key
+    UserSession.objects.create(user=user, session_key=session_key)
+
+
+user_logged_in.connect(kick_my_other_session)
