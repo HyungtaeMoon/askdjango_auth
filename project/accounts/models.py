338ab547d95ffe4aa58cf4349aa1af1e683ba741
HyungtaeMoon <blessmht@gmail.com>
2018-10-21 14:13:43 +0900

    중복 로그인 막기(미들웨어 추가)

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,13 +1,9 @@
-from importlib import import_module
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.signals import user_logged_in
 from django.db.models.signals import post_save
 from django.db import models
 from django.core.mail import send_mail
-
-
-SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
 
 
 class User(AbstractUser):
@@ -51,19 +47,5 @@ class UserSession(models.Model):
     created_at = models.DateField(auto_now_add=True)
 
 
-def kick_my_other_session(sender, request, user, **kwargs):
-    print('kicked my other login session')
-
-    for user_session in UserSession.objects.filter(user=user):
-        session_key = user_session.session_key
-        session = SessionStore(session_key)
-        # session.delete()
-        session['kicked'] = True
-        session.save()
-        user_session.delete()
-
-    session_key = request.session.session_key
-    UserSession.objects.create(user=user, session_key=session_key)
-
-
-user_logged_in.connect(kick_my_other_session)
+def on_user_logged_in(sender, request, user, **kwargs):
+    user_logged_in.connect(on_user_logged_in)
--- project/askdjango/settings.py
+++ project/askdjango/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'accounts.middleware.KickMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
