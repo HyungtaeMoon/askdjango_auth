921419d4175dff9698079cc8ae8ee2621fe04cfe
HyungtaeMoon <blessmht@gmail.com>
2019-02-02 21:52:56 +0900

    [COMMENT] ModelAdmin 의 search_fields 의 주석 추가

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -6,4 +6,5 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
     list_display_links = ['title']
+    # admin 페이지에서 키워드로 search 기능
     search_fields = ['title']
